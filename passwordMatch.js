1a0503ffadff1c73005c0bd0d975f5ff272a5b0d
Ripu <damanripu664@gmail.com>
2020-07-12 15:58:21 +0530

    passWordValidator;

---
+++ passwordMatch.js
@@ -0,0 +1,8 @@
+const validate = require('./node_modules/validator')
+const check = ((pass,cpass)=>{
+    const bool = validate.equals(pass,cpass);
+    if(bool) return 'Login Aprroved';
+    else return 'Password Mismatch';
+})
+
+module.exports = check;
