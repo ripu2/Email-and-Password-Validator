95cda742922e82df72c51f9c42a94e5b4242231f
Ripu <damanripu664@gmail.com>
2020-07-13 10:26:06 +0530

    addedChalkModule

--- util.js
+++ util.js
@@ -1,10 +1,11 @@
 const validator = require('./node_modules/validator');
+const chalk = require('./node_modules/chalk')
 const prompt = require('./node_modules/prompt-sync')();
 const check = require('./passwordMatch');
 
 const validate = ((email)=>{
     const bool = validator.isEmail(email);
-    if(!bool) return('Invalid Email');
+    if(!bool) return(chalk.redBright('Invalid Email'));
     else {
         const pass = prompt('Enter your password ');
         const cpass = prompt('Confirm your password ')
